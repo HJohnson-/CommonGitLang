1f43c1c8033adcd9ccbad4a5f55537b4c2b50289
Harry Johnson <Griffin998@hotmail.co.uk>
2015-02-09 12:25:56 +0000

    Failing test cases

---
+++ src/CommonGitLangTest.java
@@ -0,0 +1,45 @@
+import junit.framework.TestCase;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+
+/**
+ * Created by harry on 09/02/15.
+ */
+public class CommonGitLangTest extends TestCase {
+
+    @Test
+    public void testAnalyseJSONArray() throws Exception {
+        JSONAnalyzer testJSONAnalyzer = new CommonGitLang();
+
+        JSONArray example1 = new JSONArray("[]");
+        String result1 = testJSONAnalyzer.analyseJSONArray(example1);
+        assertEquals("Empty array returns null", null, result1);
+
+        JSONArray example2 = new JSONArray("[{\"language\": \"null\"}]");
+        String result2 = testJSONAnalyzer.analyseJSONArray(example2);
+        assertEquals("No repos with languages returns null", null, result2);
+
+        JSONArray example3 = new JSONArray("[{\"language\": \"Java\"}]");
+        String result3 = testJSONAnalyzer.analyseJSONArray(example3);
+        assertEquals("Single repo returns its language", "Java", result3);
+
+        JSONArray example4 = new JSONArray("[{\"language\": \"C\"}, {\"language\": \"Java\"}," +
+                " {\"language\": \"Java\"}, {\"language\": \"C\"}])");
+        String result4 = testJSONAnalyzer.analyseJSONArray(example4);
+        assertEquals("If a tie, older repos are ignored first", "Java", result4);
+
+        JSONArray example5 = new JSONArray("helloWorld");
+        String result5 = testJSONAnalyzer.analyseJSONArray(example5);
+        assertEquals("Invalid JSON returns null", null, result5);
+
+        JSONArray example6 = new JSONArray("[{\"language\": \"C\"}, {\"language\": \"Java\"}, {\"language\": \"Java\"}");
+        String result6 = testJSONAnalyzer.analyseJSONArray(example6);
+        assertEquals("The majority wins in normal situations", "Java", result6);
+
+        JSONArray example7 = new JSONArray("[{\"junk\": \"text\", \"othertext\": \"ignorethis\", \"language\": \"Java\", " +
+                "\"finaljunk\": \"finaltext\"}]");
+        String result7 = testJSONAnalyzer.analyseJSONArray(example7);
+        assertEquals("Fields except 'language' are ignored", "Java", result7);
+    }
+}
