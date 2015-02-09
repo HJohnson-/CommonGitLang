95ef61fb93ccd7652430abb2db09deb5891c43a5
Harry Johnson <Griffin998@hotmail.co.uk>
2015-02-09 11:11:02 +0000

    takes argument from user

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,8 +1,24 @@
 package src;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String gitName;
+        switch(args.length) {
+            case 0:
+                //ask user for a name
+                Scanner user_input = new Scanner( System.in );
+                gitName = user_input.next( );
+                break;
+            case 1:
+                gitName = args[0];
+                break;
+            default:
+                System.out.println("Please give 1 or 0 arguments");
+                return;
+        }
+        //AnalyseJSON(gitName);
     }
 }
